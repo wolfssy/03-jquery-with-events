0e80654844f24d459f5b24ee523b1a08599853fc
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-08-16 15:41:21 -0700

    added nav handler and working

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -55,20 +55,42 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
+  // TODO:(done) Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
-
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      var $category = $(this).val();
+      $('article[data-category = "' + $category+'"]').show();
+    } else {
+      (!$(this).val())
+      $('article').show();
+      $('article.template').hide();
+    }
+    $('#author-filter').val('');
+  });
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+  // TODO: (done) Add an event handler to .main-nav elements that will power the Tabs feature.
   //       Clicking any .tab element should hide all the .tab-content sections, and then reveal the
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
   //       data available to you on the .tab element that was clicked.
-
+  $('.main-nav .tab').on('click', function() {
+    var $navValue = $(this).data('content');
+    if ($navValue === 'articles') {
+      console.log($navValue);
+      $('article').show();
+      $('article.template').hide();
+      $('section#about').hide();
+    } else if ($navValue === 'about') {
+      $('article').hide();
+      $('section#about').show();
+    }
+  })
 
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
@@ -90,4 +112,6 @@ articleView.setTeasers = function() {
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
+  articleView.handleCategoryFilter();
+  articleView.handleMainNav();
 })
